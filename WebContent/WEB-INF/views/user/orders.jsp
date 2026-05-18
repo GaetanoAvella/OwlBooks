d1680359823412f01006e76ce9ac5123084d5786
gaetano <avellagaetano2001@gmail.com>
2026-05-18 11:12:17 +0200

    Some progress with order.jsp

--- WebContent/WEB-INF/views/user/orders.jsp
+++ WebContent/WEB-INF/views/user/orders.jsp
@@ -1,12 +1,24 @@
+<%@page import="it.unisa.model.PurchaseOrderBean"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="it.unisa.model.UserBean"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Ordini</title>
 </head>
 <body>
 
+	<% 
+	UserBean user = (UserBean) session.getAttribute("user"); 
+	ArrayList<PurchaseOrderBean> orders = (ArrayList<PurchaseOrderBean>) request.getAttribute("orders"); 
+	for(PurchaseOrderBean order : orders) { %>
+		<ul>
+			<li>Ordine <a href="OrderServlet/<%= order.getOrderCode() %>"><%= order.getOrderCode() %></a></li>
+		</ul>
+		
+	<% } %>
 </body>
 </html>
--- src/it/unisa/control/user/ProfileServlet.java
+++ src/it/unisa/control/user/ProfileServlet.java
@@ -1,6 +1,7 @@
 package it.unisa.control.user;
 
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,10 +9,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import it.unisa.model.PurchaseOrderBean;
+import it.unisa.model.UserBean;
+import it.unisa.storage.order.PurchaseOrderDao;
+import it.unisa.storage.order.PurchaseOrderDaoImpl;
 
 @WebServlet("/ProfileServlet")
 public class ProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private PurchaseOrderDao dao;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		DataSource ds = (DataSource) getServletContext().getAttribute("datasource");
+		if(ds == null)
+			throw new ServletException("Datasource non disponibile nel contest");
+		
+		dao = new PurchaseOrderDaoImpl(ds);
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String profileForm = request.getParameter("profile");
@@ -23,6 +44,13 @@ public class ProfileServlet extends HttpServlet {
 				dispatcher.forward(request, response);
 				break;
 			case "orders":
+				UserBean user = (UserBean) request.getSession().getAttribute("user");
+				try {
+					ArrayList<PurchaseOrderBean> orders = dao.doRetriveByUser(user.getId());
+					request.setAttribute("orders", orders);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 				dispatcher = request.getRequestDispatcher("WEB-INF/views/user/orders.jsp");
 				dispatcher.forward(request, response);
 				break;
